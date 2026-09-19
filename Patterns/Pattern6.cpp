c36be522c993a298dae4cd727b8079154e8e6165
Neha Sharma <nehasharma147@outlook.com>
2026-09-19 23:35:18 +0530

    Added new C++ DSA logic building practice problems

--- Patterns/Pattern6.cpp
+++ Patterns/Pattern6.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n=4;
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
     for(int i=1;i<=n;i++){
         cout<<" * ";
     for(int j=1;j<=n-1;j++){
